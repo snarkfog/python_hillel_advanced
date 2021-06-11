5ec9d014668bbad485c1e8ad548a2ef7bc6ae700
Snarkfog <snarkfog@gmail.com>
2021-06-11 16:40:34 +0300

    Lesson 06 homework complete

---
+++ lesson_06/hw_06_shape.py
@@ -0,0 +1,25 @@
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Point(Shape):
+    pass
+
+
+class Circle(Shape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def contains(self, point):
+        if ((point.x - self.x) ** 2 + (point.y - self.y) ** 2) <= self.radius ** 2:
+            return True
+        else:
+            return False
+
+
+point = Point(50, 50)
+circle = Circle(50, 50, 100)
+print(circle.contains(point))
